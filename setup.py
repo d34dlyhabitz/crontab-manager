bdf7229b4b14c371f004a615500c26eb9cbb715f
d34dlyhabitz <d34dlyhabitz@protonmail.com>
2020-09-10 11:56:57 +0300

    project initialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='crontab-manager',
+    packages=find_packages(),
+    version='0.0.1'
+    description='easy to use library for cron jobs management',
+    author='d34dlyhabitz',
+    license='WTFPL'
+)
